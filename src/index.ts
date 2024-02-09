b99fc6f0275afa78bce4078be8857bfab043bcd3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-02-09 21:25:12 +0530

    added logic to clone repo , get file path and upload to R2

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,22 @@ import express from "express"
 import cors from "cors"
 import simpleGit from "simple-git"
 import { generate } from "./utils"
-
+import { getPath } from "./files"
+import path from 'path'
+import { uploadFile } from "./aws"
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.post('/sendRepoUrl',async(req,res)=>{
     const repoUrl = req.body.repoUrl
     const id = generate()
-    await simpleGit().clone(repoUrl , `output/${id}`)
-    res.json({})
+    await simpleGit().clone(repoUrl , path.join(__dirname ,`output/${id}`) )
+    const files =  getPath(path.join(__dirname , `output/${id}`))
+    console.log(files);
+    files.forEach(async file => {
+        await uploadFile(file.slice(__dirname.length + 1) , file)
+    })
+    res.json({id:id})
 })
 app.listen(3000 , ()=>{
     console.log("port listining on port  3000")
